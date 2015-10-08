d4273fd5fc4c125ecdf3f663452ee3bc6ba07f06
davidjcmar <davidjcmar@gmail.com>
2015-10-07 21:49:46 -0500

    ""

--- server.c
+++ server.c
@@ -93,7 +93,7 @@ int chk_cache (CACHE** cache_list, char* message)
 	char buffer[strlen(message)];
 	memset (buffer,'\0',strlen(buffer));
 	strcpy (buffer,message);
-	buffer[strlen(buffer)-1]='\n';
+	buffer[strlen(buffer)-1]='\0';
 	for (i=0;i<5;i++)
 	{
 		printf ("%d %d %s\n",strlen(buffer),strlen(cache_list[i]->url), cache_list[i]->url);
